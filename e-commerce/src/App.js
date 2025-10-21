add89c19e9f0d3746aa54f1c8d154fdb4761ccdf
Potchibels <johnmichaelgigante26@gmail.com>
2025-10-21 21:00:54 +0800

    add the Product.js , ProductCard.js/css , ProductDetails.css/js , ProductList.css/js, and connect all the route in app.js

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </>
   );
